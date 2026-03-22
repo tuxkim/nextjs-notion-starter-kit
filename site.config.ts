19c1dd6aabc808bb0165751da757e874558a2777
tuxkim <tux.kim@kakaounion.com>
2026-03-22 22:43:03 +0900

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '8c8e211595f648eda45b5091dde23522',
+  rootNotionPageId: '32b28e5a33f0806199bdfacd70753419',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
